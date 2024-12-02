a2c5d6127f17e2161d5ae2af6343c3988e467be8
0xguava <demo.lappy@protonmail.com>
2024-12-02 17:58:47 +0530

    begun changing earlier

--- ubuntu/v22.04/2. Services/2.1 Configure Server Services/server-services-chk.sh
+++ ubuntu/v22.04/2. Services/2.1 Configure Server Services/server-services-chk.sh
@@ -5,24 +5,27 @@ service-chk() {
   local output_p=''
   local output_f=''
   local logvr=1
+  local pkg_name=$(echo $1 | cut -d\. -f1)
 
-  if dpkg-query -s autofs &>/dev/null; then
-    pkg_output="$pkg_output\n\t - autofs is installed."
+  if dpkg-query -s "$pkg_name" &>/dev/null; then
+    pkg_output="$pkg_output\n\t - $pkg_name is installed."
   else
-    pkg_output="$pkg_output\n\t - autofs isn't installed."
+    pkg_output="$pkg_output\n\t - $pkg_name isn't installed."
   fi
 
-  if systemctl is-enabled autofs.service 2>/dev/null | grep -q 'enabled'; then
-    output_f="$output_f\n\t - autofs.service is enabled."
+  if systemctl is-enabled "$1" 2>/dev/null | grep -q 'enabled'; then
+    output_f="$output_f\n\t - $1 is enabled."
     logvr=0
   else
-    output_p="$output_p\n\t - autofs.service is NOT enabled."
+    output_p="$output_p\n\t - $1 is NOT enabled."
   fi
 
-  if systemctl is-active autofs.service 2>/dev/null | grep -q '^active'; then
-    output_f="$output_f\n\t - autofs.service is active."
+  if systemctl is-active "$1" 2>/dev/null | grep -q '^active'; then
+    output_f="$output_f\n\t - $1 is active."
     logvr=0
   else
-    output_p="$output_p\n\t - autofs.service is NOT active."
+    output_p="$output_p\n\t - $1 is NOT active."
   fi
+
+
 }
