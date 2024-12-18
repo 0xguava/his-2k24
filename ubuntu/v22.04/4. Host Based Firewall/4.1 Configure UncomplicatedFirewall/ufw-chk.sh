9b10dbe59661faba17f441320d6eb65a65a18b5c
0xguava <demo.lappy@protonmail.com>
2024-12-18 22:44:30 +0530

    last but not the least commit in this particular repo

--- ubuntu/v22.04/4. Host Based Firewall/4.1 Configure UncomplicatedFirewall/ufw-chk.sh
+++ ubuntu/v22.04/4. Host Based Firewall/4.1 Configure UncomplicatedFirewall/ufw-chk.sh
@@ -116,8 +116,162 @@ ufw_rules_chk() {
   fi
 }
 
+firewall_rule_chk() {
+
+  echo -e "- Firewall rule check for open ports:"
+
+  local output_p=''
+
+  local output_f=''
+
+  local logvr=1
+
+
+  # Get UFW open ports
+
+  local a_ufwout=()
+
+  while read -r l_ufwport; do
+
+    [ -n "$l_ufwport" ] && a_ufwout+=("$l_ufwport")
+
+  done < <(ufw status verbose | grep -Po '^\h*\d+\b' | sort -u)
+
+
+  # Get open ports
+
+  local a_openports=()
+
+  while read -r l_openport; do
+
+    [ -n "$l_openport" ] && a_openports+=("$l_openport")
+
+  done < <(ss -tuln | awk '($5!~/%lo:/ && $5!~/127.0.0.1:/ &&  $5!~/\[?::1\]?:/) {split($5, a, ":"); print a[2]}' | sort -u)
+
+
+  # Find ports without UFW rules
+
+  local a_diff=($(printf '%s\n' "${a_openports[@]}" "${a_ufwout[@]}" "${a_ufwout[@]}" | sort | uniq -u))
+
+
+  if [[ -n "${a_diff[*]}" ]]; then
+
+    output_f="$output_f\n\t - The following port(s) don't have a rule in UFW: $(printf '%s\n' "${a_diff[*]}")"
+
+    logvr=0
+
+  else
+
+    output_p="$output_p\n\t - All open ports have a rule in UFW"
+
+  fi
+
+
+  if [[ $logvr -eq 0 ]]; then
+
+    echo -e "\t- Audit result: **FAIL** [Firewall rule is not set correctly]"
+
+    echo -e "\t- Reason: $output_f"
+
+  else
+
+    echo -e "\t- Audit result: **PASS** [Firewall rule is set correctly]"
+
+    echo -e "\t- Reason: $output_p"
+
+  fi
+
+}
+
+default_policy_chk() {
+
+  echo -e "- Default policy check for incoming, outgoing, and routed directions:"
+
+  local output_p=''
+
+  local output_f=''
+
+  local logvr=1
+
+
+  # Get default policy
+
+  local default_policy=$(ufw status verbose | grep Default:)
+
+
+  # Check incoming policy
+
+  if echo "$default_policy" | grep -q "deny (incoming)"; then
+
+    output_p="$output_p\n\t - Incoming policy is set to deny"
+
+  elif echo "$default_policy" | grep -q "reject (incoming)"; then
+
+    output_p="$output_p\n\t - Incoming policy is set to reject"
+
+  else
+
+    output_f="$output_f\n\t - Incoming policy is not set to deny or reject"
+
+    logvr=0
+
+  fi
+
+
+  # Check outgoing policy
+
+  if echo "$default_policy" | grep -q "deny (outgoing)"; then
+
+    output_p="$output_p\n\t - Outgoing policy is set to deny"
+
+  elif echo "$default_policy" | grep -q "reject (outgoing)"; then
+
+    output_p="$output_p\n\t - Outgoing policy is set to reject"
+
+  else
+
+    output_f="$output_f\n\t - Outgoing policy is not set to deny or reject"
+
+    logvr=0
+
+  fi
+
+
+  # Check routed policy
+
+  if echo "$default_policy" | grep -q "disabled (routed)"; then
+
+    output_p="$output_p\n\t - Routed policy is set to disabled"
+
+  else
+
+    output_f="$output_f\n\t - Routed policy is not set to disabled"
+
+    logvr=0
+
+  fi
+
+
+  if [[ $logvr -eq 0 ]]; then
+
+    echo -e "\t- Audit result: **FAIL** [Default policy is not set correctly]"
+
+    echo -e "\t- Reason: $output_f"
+
+  else
+
+    echo -e "\t- Audit result: **PASS** [Default policy is set correctly]"
+
+    echo -e "\t- Reason: $output_p"
+
+  fi
+
+}
 
 iptables-ins-chk
 ufw-installed-chk
 enabled-chk 
 ufw_rules_chk
+firewall_rule_chear
+default_policy_chk
+
